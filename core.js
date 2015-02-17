0a8dab878530c9e45cf97d8048592186680d5a35
Byrdann Fox <mobileupstate@gmail.com>
2015-02-17 09:05:58 -0500

    Updates...

--- core.js
+++ core.js
@@ -4,6 +4,26 @@
 
 	console.log(JS);
 
-	
+	// some of the different types of values in JavaScript...
+	var num = 20;
+	num += 4;
+	console.log(num);
+
+	var price = 1.50;
+	console.log(price);
+
+	var name = 'Poplin';
+	console.log(name);
+
+	var trueValue = true;
+	console.log(trueValue);
+
+	// no value...
+	var nullValue = null;
+	console.log(nullValue);
+
+	// ready to be assigned...
+	var und;
+	console.log(und);
 
 }("JavaScript..."));
